b65d168ea06ff2ccfd65b9d79d7aaf82d74fcb0d
Daniel Ochoa <ochoad24@gmail.com>
2019-10-23 19:57:46 -0600

    Agregar SW

---
+++ public/js/push.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const swReady = navigator.serviceWorker.ready;
+
+function initPush() {
+    if (!swReady) {
+        return;
+    }
+
+    new Promise(function (resolve, reject) {
+        const permissionResult = Notification.requestPermission(function (result) {
+            resolve(result);
+        });
+
+        if (permissionResult) {
+            permissionResult.then(resolve, reject);
+        }
+    })
+        .then((permissionResult) => {
+            if (permissionResult !== 'granted') {
+                throw new Error('We weren\'t granted permission.');
+            }
+            subscribeUser();
+        });
+}
+
+/**
+ * Subscribe the user to push
+ */
+function subscribeUser() {
+    swReady
+        .then((registration) => {
+            const subscribeOptions = {
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array(
+                    'BAEq0_5uF3i-juQkWJZzsotDLL4kAUsaFt94jkwm7qyRzlHUVsy0mfauWXRtB_rrxnuBxVmggbyPthq_O5jArOk'
+                )
+            };
+
+            return registration.pushManager.subscribe(subscribeOptions);
+        })
+        .then((pushSubscription) => {
+            console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+            storePushSubscription(pushSubscription);
+        });
+}
+
+/**
+ * send PushSubscription to server with AJAX.
+ * @param {object} pushSubscription
+ */
+function storePushSubscription(pushSubscription) {
+    fetch('api/push', {
+        method: 'POST',
+        body: JSON.stringify(pushSubscription),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': token
+        }
+    })
+        .then((res) => {
+            return res.json();
+        })
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+}
+
+/**
+ * urlBase64ToUint8Array
+ * 
+ * @param {string} base64String a public vapid key
+ */
+function urlBase64ToUint8Array(base64String) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
